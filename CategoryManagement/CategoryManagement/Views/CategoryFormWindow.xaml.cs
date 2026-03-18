de4bf6cb03480efd66e4a45236dd82e28b1176f4
KartikZCoding <ahir.kartik.385@gmail.com>
2026-03-18 22:07:18 +0530

    Views :- add MainWindow, CategoryFormView and CategoryFormWindow UI

---
+++ CategoryManagement/CategoryManagement/Views/CategoryFormWindow.xaml.cs
@@ -0,0 +1,15 @@
+using System.Windows;
+
+namespace CategoryManagement.Views
+{
+    /// <summary>
+    /// Interaction logic for CategoryFormWindow.xaml
+    /// </summary>
+    public partial class CategoryFormWindow : Window
+    {
+        public CategoryFormWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
